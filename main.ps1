caf753a774b6d68f9415da0cabff7998578616a3
rhattox <bcovies@gmail.com>
2023-12-19 12:35:18 -0300

    updated winget

--- main.ps1
+++ main.ps1
@@ -11,44 +11,115 @@
 
 
 # Install applications using winget with specified versions
-
+############################################################################
 # Applications
-$apps = @{
-    "Google.Chrome" = "88.0.4324.182";
-    "Microsoft.VisualStudioCode" = "1.60.2";
-    "Notepad++" = "8.1.9";
-    "Docker" = "20.10.10";
-    "Git.Git" = "2.33.0";
-    "GitHub.GitHubDesktop" = "2.9.4";
-    "7zip.7zip" = "19.00";
-    "Discord" = "0.0.308";
-    "Steam" = "2.10.91.91";
-    "qbittorrent.qbittorrent" = "4.3.8";
-    "VLC" = "3.0.12";
-    "Postman" = "9.11.5";
-    "Notion" = "2.10.11";
-    "NVIDIAGeForceExperience.NVIDIAGeForceExperience" = "3.24.0.70";
-    "Amazon.AWSCLI" = "2.2.38";
-    "Google.CloudSDK" = "361.0.0";
-    "Microsoft.AzureCLI" = "2.28.0"
+$apps = @(
+    "Google.Chrome",
+    "WhatsApp.WhatsApp",
+    "Mozilla.Firefox",
+    "Telegram.TelegramDesktop",
+    "Spotify.Spotify", 
+    "Microsoft.VisualStudioCode",
+    "Notepad++.Notepad++",
+    "WinSCP.WinSCP",
+    "Microsoft.WindowsTerminal",
+    "PuTTY.PuTTY",
+    "Postman.Postman",
+    "Git.Git",,
+    "7zip.7zip",
+    "VideoLAN.VLC",
+    "Notion.Notion",
+    "Valve.Steam",
+    "Discord.Discord",
+    "ElectronicArts.Origin",
+    "EpicGames.EpicGamesLauncher",
+    "RiotGames.LeagueOfLegends.BR",
+    "qBittorrent.qBittorrent",
+    "Nvidia.GeForceExperience"
+)
+
+# Install Tools
+foreach ($item in $apps) {
+    Write-Host "`n"
+    $Key = $item
+    $Path = "$env:USERPROFILE\Custom\$Key"
+    Write-Host "App: $Key,  Path: $Path"
+    winget install --location $Path --id $Key -e
+    Write-Host "`n"
 }
+############################################################################
+# Infra Tools
+$infra = @(
+    "Amazon.AWSCLI",
+    "Google.CloudSDK",
+    "Microsoft.AzureCLI",
+    "GitHub.GitHubDesktop",
+    "Docker.DockerDesktop",
+    "Docker.DockerCLI",
+    "Docker.DockerCompose",
+    "RedHat.Podman"
+)
+
+# Install Infra Tools
+foreach ($item in $infra) {
+    Write-Host "`n"
+    $Key = $item
+    $Path = "$env:USERPROFILE\Custom\$Key"
+    Write-Host "App: $Key,  Path: $Path"
+    winget install --location $Path --id $Key -e
+    Write-Host "`n"
+}
+
+############################################################################
+# Hashicorp Tools
+$hashicorp = @(
+    "Hashicorp.Boundary",
+    "Hashicorp.Consul",
+    "Hashicorp.Nomad",
+    "Hashicorp.Packer",
+    "Hashicorp.Vagrant",
+    "Hashicorp.Waypoints",
+    "Hashicorp.Terraform",
+    "Hashicorp.Vault"
+)
 
 # Development Tools
-$tools = @{
-    "Node.Nodejs" = "14.17.6";
-    "Golang.Go" = "1.17.1";
-    "Python.Python" = "3.9.7";
-    "OpenJDK.JavaRuntime" = "11.0.12";
-    "Rustlang.Rust" = "1.55.0";
-    "Ruby.Ruby" = "3.0.2"
+$DevKit = @{
+    # LTS Version
+    "OpenJS.NodeJS.LTS" = "20.10.0";
+    # Latest Version
+    "Golang.Go" = "1.21.5";
+    # Gets the ID version
+    "Python.Python.3.12" = "3.12";
+    # Matching version For Eclipse
+    "EclipseAdoptium.Temurin.20.JRE" = "20.0.1.9";
+    "EclipseAdoptium.Temurin.20.JDK" = "20.0.1.9";
+    # Fixed version
+    "Rustlang.Rustup" = "1.26.0";
+    # Fixed version
+    "CrystalLang.Crystal" = "1.9.2"
+    # Gets the ID version
+    "RubyInstallerTeam.Ruby.3.2 " = "3.2.1-1"
 }
 
+# Install DevKit
+foreach ($kit in $DevKit.GetEnumerator()) {
+    Write-Host "`n"
+    $Key = $kit.Key
+    $Value = $kit.Value
+    $Path = "$env:USERPROFILE\Custom\$Key"
+    Write-Host "DevKit: $Key, Version: $Value, Path: $Path"
+    winget install --location $Path --id $Key --version $Value -e
+    Write-Host "`n"
+}
+
+############################################################################
 # IDEs
 $ides = @{
     "JetBrains.CLion" = "2021.1.3";
     "JetBrains.DataGrip" = "2021.1.3";
-    "JetBrains.GoLand" = "2021.2.3";
-    "JetBrains.IdeaU" = "2021.1.2";
+    "JetBrains.GoLand" = "2021.1.3";
+    "JetBrains.IntelliJIDEA.Ultimate" = "2021.1.2";
     "JetBrains.Rider" = "2021.1.5";
     "JetBrains.PhpStorm" = "2021.1";
     "JetBrains.PyCharm.Professional" = "2021.2.2";
@@ -56,25 +127,17 @@ $ides = @{
     "JetBrains.WebStorm" = "2021.1.3"
 }
 
-# # Install Applications
-# foreach ($app in $apps.GetEnumerator()) {
-#     Write-Host "$app.Key"
-#     winget install --location "$env:USERPROFILE\Custom\$app.Key" --id $app.Key --version $app.Value -e
-# }
 
-# # Install Development Tools
-# foreach ($tool in $tools.GetEnumerator()) {
-#     Write-Host "$tool.Key"
-#     winget install --location "$env:USERPROFILE\Custom\$tool.Key" --id $tool.Key --version $tool.Value -e
-# }
 
 # Install IDEs
 foreach ($ide in $ides.GetEnumerator()) {
-    $ideKey = $ide.Key
-    $ideValue = $ide.Value
-    $idePath = "$env:USERPROFILE\Custom\$ideKey"
-    Write-Host "IDE: $ideKey, Version: $ideValue, Path: $idePath"
-    winget install --location $idePath --id $ideKey --version $ideValue -e
+    Write-Host "`n"
+    $Key = $ide.Key
+    $Value = $ide.Value
+    $Path = "$env:USERPROFILE\Custom\$Key"
+    Write-Host "IDE: $Key, Version: $Value, Path: $Path"
+    winget install --location $Path --id $Key --version $Value -e
+    Write-Host "`n"
 }
 
 
