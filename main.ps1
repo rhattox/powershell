c8fa147a2686c893786760c7ce832ba9ce1c3392
Bruno Souto <bcovies@gmail.com>
2024-09-10 22:04:55 -0300

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,142 +1,120 @@
 # Install applications using winget with specified versions
-############################################################################
-# Applications
-$apps = @(
+
+# To install applications that the hash doesn't match, use this
+# winget settings --enable InstallerHashOverride
+
+# remove the UAC from windows panel to make it easier
+# put it back on once it's done
+
+$installUserApps = $false
+$installGamerApps = $false
+$installDevApps = $true
+$installContainersApps = $true
+$installHashicorpApps = $true
+#
+# User
+#
+$user = @(
     "Google.Chrome",
-    "WhatsApp.WhatsApp",
     "Mozilla.Firefox",
-    "Telegram.TelegramDesktop",
+    "9NKSQGP7F2NH", #whatsapp
     "Spotify.Spotify", 
-    "Microsoft.VisualStudioCode",
-    "Notepad++.Notepad++",
-    "WinSCP.WinSCP",
-    "Microsoft.WindowsTerminal",
-    "PuTTY.PuTTY",
-    "Postman.Postman",
-    "Git.Git",,
+    "Telegram.TelegramDesktop",
     "7zip.7zip",
     "VideoLAN.VLC",
-    "Notion.Notion",
+    "qBittorrent.qBittorrent"
+)
+#
+# Gamer
+#
+$gamer = @(
     "Valve.Steam",
     "Discord.Discord",
     "ElectronicArts.Origin",
     "EpicGames.EpicGamesLauncher",
-    "RiotGames.LeagueOfLegends.BR",
-    "qBittorrent.qBittorrent",
-    "Microsoft.PowerShell",
-    "Nvidia.GeForceExperience"
+    "Nvidia.GeForceExperience",
+    "CPUID.HWMonitor",
+    "Guru3D.Afterburner"
 )
 
-# Install Tools
- foreach ($item in $apps) {
-     Write-Host "`n"
-     $Key = $item
-     $Path = "$env:USERPROFILE\Custom\$Key"
-     Write-Host "App: $Key,  Path: $Path"
-     winget install --location $Path --id $Key -e
-     Write-Host "`n"
- }
-
-############################################################################
-# Infra Tools
-$infra = @(
-    "Amazon.AWSCLI",
-    "Google.CloudSDK",
-    "Microsoft.AzureCLI",
+#
+# Developer
+#
+$dev = @(
+    "Notion.Notion",
+    "Microsoft.VisualStudioCode",
+    "Notepad++.Notepad++",
+    "PuTTY.PuTTY",
+    "WinSCP.WinSCP",
+    "Git.Git",
+    "Microsoft.WindowsTerminal",
+    "Microsoft.PowerShell",
     "GitHub.GitHubDesktop",
+    "GitHub.cli"
+    "Oracle.JDK.22",
+    "JetBrains.IntelliJIDEA.Ultimate",
+    "Microsoft.VisualStudio.2022.Community",
+    "Debian.Debian"
+)
+    
+
+$containers = @(
     "Docker.DockerDesktop",
     "Docker.DockerCLI",
     "Docker.DockerCompose",
-    "RedHat.Podman"
+    "Kubernetes.kubectl",
+    "Kubernetes.minikube",
+    "Helm.Helm",
+    "Mirantis.Lens",
+    "ahmetb.kubectx",
+    "ahmetb.kubens"
+    # "argoproj.argocd"
 )
 
-# Install Infra Tools
- foreach ($item in $infra) {
-     Write-Host "`n"     
-     $Key = $item
-     $Path = "$env:USERPROFILE\Custom\$Key"
-     Write-Host "App: $Key,  Path: $Path"
-     winget install --location $Path --id $Key -e
-     Write-Host "`n"
- }
-
-############################################################################
-# Hashicorp Tools
 $hashicorp = @(
-    "Hashicorp.Boundary",
-    "Hashicorp.Consul",
-    "Hashicorp.Nomad",
     "Hashicorp.Packer",
     "Hashicorp.Vagrant",
-    "Hashicorp.Waypoints",
-    "Hashicorp.Terraform",
-    "Hashicorp.Vault"
+    "Hashicorp.Terraform"
 )
 
-# Development Tools
-$DevKit = @{
-    # LTS Version
-    "OpenJS.NodeJS.LTS" = "20.10.0";
-    # Latest Version
-    # Gets the ID version
-    "Python.Python.3.12" = "3.12";
-    # Matching version For Eclipse
-    "EclipseAdoptium.Temurin.20.JRE" = "20.0.1.9";
-    "EclipseAdoptium.Temurin.20.JDK" = "20.0.1.9";
-    # Fixed version
-    "Rustlang.Rustup" = "1.26.0";
-    # Fixed version
-    "CrystalLang.Crystal" = "1.9.2"
-    # Gets the ID version
-    "RubyInstallerTeam.Ruby.3.2" = "3.2.1-1"
+
+function InstallApps {
+    param (
+        [string[]]$apps
+    )
+    # Install Tools
+    foreach ($item in $apps) {
+        Write-Host "`n"
+        $Key = $item
+        $Path = "$env:USERPROFILE\Custom\$Key"
+        Write-Host "App: $Key,  Path: $Path"
+        winget install --location $Path --id $Key -e --accept-package-agreements --force --disable-interactivity --silent  --ignore-security-hash
+        Write-Host "`n"
+    }
 }
 
-# Install DevKit
-# foreach ($kit in $DevKit.GetEnumerator()) {
-#     Write-Host "`n"
-#     $Key = $kit.Key
-#     $Value = $kit.Value
-#     $Path = "$env:USERPROFILE\Custom\$Key"
-#     Write-Host "DevKit: $Key, Version: $Value, Path: $Path"
-#     winget install --location $Path --id $Key --version $Value -e
-#     Write-Host "`n"
-# }
-
-############################################################################
-# IDEs
-$ides = @{
-    "JetBrains.CLion" = "2021.1.3";
-    "JetBrains.DataGrip" = "2021.1.3";
-    "JetBrains.GoLand" = "2021.1.3";
-    "JetBrains.IntelliJIDEA.Ultimate" = "2021.1.2";
-    "JetBrains.Rider" = "2021.1.5";
-    "JetBrains.PhpStorm" = "2021.1.1";
-    "JetBrains.PyCharm.Professional" = "2021.2.2";
-    "JetBrains.RubyMine" = "2021.1.2";
-    "JetBrains.WebStorm" = "2021.1.3"
+if ($installUserApps) {
+    InstallApps -apps $user
+}
+if ($installGamerApps) {
+    InstallApps -apps $gamer
+}
+if ($installDevApps) {
+    InstallApps -apps $dev
+}
+if ($installContainersApps) {
+    InstallApps -apps $containers
+}
+if ($installHashicorpApps) {
+    InstallApps -apps $hashicorp
 }
 
+# Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient -NoRestart
+# Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+# Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -NoRestart
+# Enable-WindowsOptionalFeature -Online -FeatureName Containers -NoRestart
+# Enable-WindowsOptionalFeature -Online -FeatureName Containers-DisposableClientVM -All 
 
+# New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -PropertyType DWORD -Force
 
-# Install IDEs
- # foreach ($ide in $ides.GetEnumerator()) {
- #     Write-Host "`n"
- #     $Key = $ide.Key
- #     $Value = $ide.Value
- #     $Path = "$env:USERPROFILE\Custom\$Key"
- #     Write-Host "IDE: $Key, Version: $Value, Path: $Path"
- #     winget install --location $Path --id $Key --version $Value -e
- #     Write-Host "`n"
- # }
-
-
-
-Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient -NoRestart
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -NoRestart
-Enable-WindowsOptionalFeature -Online -FeatureName Containers -NoRestart
-Enable-WindowsOptionalFeature -Online -FeatureName Containers-DisposableClientVM -All 
-
-New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -PropertyType DWORD -Force
-
-New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarSmallIcons -Value 1 -PropertyType DWORD -Force
+# New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarSmallIcons -Value 1 -PropertyType DWORD -Force
